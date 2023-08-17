e3969d30635f24023ad68c15990da5c3dba2b35f
tenlhagyal1 <lhagyaltenzing20@gmail.com>
2023-08-17 15:59:33 -0400

    done with CSS,HTML,JS

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lost and Loose
+Get ready for a game set somewhere out in space! An astronaut is floating around space and you must guess the word correctly to keep him hidden. Every wrong letter that you guess, a part of his body will start to appear but becareful the max amount of wrong guesses allowed will be six. You must guess the word correctly before that, there will be hints provided to help solve this puzzle. Do you have what it takes to complete this mission? 
+
+## Screenshot
+
+### Starting Screen
+![Starting Screen](Initial-View.png)
+
+### Ending Screen
+![Winning Screen](Win-view.png)
+
+
+
+# Technologies Used
+
+- JavaScript
+- HTML
+- CSS
+
+# Getting Started
--- main.js
+++ main.js
@@ -1,49 +1,153 @@
 /*----- constants -----*/
 const wordsWithHints = [
-    {word: 'Variable', hint: 'A placeholder for data in coding'},
-    {word: 'Array', hint: 'A data structure used to store an ordered "list" of data'},
-    {word: 'Push', hint: 'Adds elements to an end of an array'},
-    {word: 'Pop', hint: 'Removes a single element from the front of an array'},
-    {word: 'Splice', hint: 'Capable of adding or removing any number of elements to or from an array'},
-    {word: 'Function', hint: 'Reusable block of code written to perform a single purpose'},
-    {word: 'Parameter', hint: 'Are placeholders when the function is defined that accepts data'},
-    {word: 'Arguments', hint: 'Are data being passed to the function when the function is called'},
-    {word: 'Pseudocode', hint: 'Outlines the apps logic using plain language'},
-    {word: 'Wireframes', hint: 'Provides a blueprint for the HTML and CSS'},
-    {word: 'forEach', hint: 'General purpose iterator method'},
-    {word: 'Map', hint: 'Create a new array from a source array by replacing or transforming its elements'},
-    {word: 'Filter', hint: 'Select certain elements from a source array'},
-    {word: 'Find', hint: 'Finds an element within an array'},
-    {word: 'FindIndex', hint: 'Finds an element within an array but returns the elements index'},
-    {word: 'Some', hint: 'Checks if an array has atleast one elemnt that meets a certain condition'},
-    {word: 'Unshift', hint: 'Adds elements to the front of an array'},
-    {word: 'Shift', hint: 'Removes a single element from the front of an array'},
-    {word: 'Every', hint: 'Checks if every element in the array meets a certain condition'},
-    {word: 'Classes', hint: 'Used to create objects'},
+    { word: 'Variable', hint: 'A placeholder for data in coding' },
+    { word: 'Array', hint: 'A data structure used to store an ordered "list" of data' },
+    { word: 'Push', hint: 'Adds elements to an end of an array' },
+    { word: 'Pop', hint: 'Removes a single element from the front of an array' },
+    { word: 'Splice', hint: 'Capable of adding or removing any number of elements to or from an array' },
+    { word: 'Function', hint: 'Reusable block of code written to perform a single purpose' },
+    { word: 'Parameter', hint: 'Are placeholders when the function is defined that accepts data' },
+    { word: 'Arguments', hint: 'Are data being passed to the function when the function is called' },
+    { word: 'Pseudocode', hint: 'Outlines the apps logic using plain language' },
+    { word: 'Wireframes', hint: 'Provides a blueprint for the HTML and CSS' },
+    { word: 'forEach', hint: 'General purpose iterator method' },
+    { word: 'Map', hint: 'Create a new array from a source array by replacing or transforming its elements' },
+    { word: 'Filter', hint: 'Select certain elements from a source array' },
+    { word: 'Find', hint: 'Finds an element within an array' },
+    { word: 'FindIndex', hint: 'Finds an element within an array but returns the elements index' },
+    { word: 'Some', hint: 'Checks if an array has atleast one element that meets a certain condition' },
+    { word: 'Unshift', hint: 'Adds elements to the front of an array' },
+    { word: 'Shift', hint: 'Removes a single element from the front of an array' },
+    { word: 'Every', hint: 'Checks if every element in the array meets a certain condition' },
+    { word: 'Classes', hint: 'Used to create objects' },
 ];
 
-/*----- state variables -----*/
+// Length 7
+const imageList = [
+    "astronauts/astro-6.png", // index 0
+    "astronauts/astro-5.png", // index 1
+    "astronauts/astro-4.png", // index 2
+    "astronauts/astro-3.png", // index 3
+    "astronauts/astro-2.png", // index 4
+    "astronauts/astro-1.png", // index 5
+    "astronauts/astro-0.png", // index 6
+]
 
+const MAX_INCORRECT_GUESSES = 6;
+
+/*----- state variables -----*/
+let currentImageIndex = 0;
+let incorrectGuessesList = [];
+let correctGuessesList = [];
+let randomWordOriginalCase;
+let randomWordLowerCase;
 
 /*----- cached elements  -----*/
+const notification = document.getElementById("notification");
 
 
 /*----- event listeners -----*/
-
+//the main function is called when the window is laoded
+window.onload = main;
+document.getElementById("enter-button").addEventListener("click", checkGuess);
+document.getElementById("restart-button").addEventListener("click", function () {
+    location.reload();
+});
+//event listener for my enter key
+document.getElementById("letter-guess").addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+        checkGuess(e);
+    }
+});
 
 /*----- functions -----*/
-
 // Function to pick a random word and set the associated hint
 function pickRandomWordAndSetHint() {
-    // Pick a random word with hint
     const randomIndex = Math.floor(Math.random() * wordsWithHints.length);
     const randomWordWithHint = wordsWithHints[randomIndex];
-    // Set the word display with underscores 
-    document.getElementById('word-display').textContent = '_ '.repeat(randomWordWithHint.word.length);
+    randomWordOriginalCase = randomWordWithHint.word;
+    //this line changes the word to lowercase and assigns to randomWordLowerCase
+    randomWordLowerCase = randomWordWithHint.word.toLowerCase();
 
-    // Set the associated hint in the hint-box
+    // Sets the word display and hint
+    document.getElementById('word-display').textContent = '_'.repeat(randomWordWithHint.word.length);
     document.querySelector('#hint-box span').textContent = randomWordWithHint.hint;
 }
 
-// Call the function to set the word and hint on page load
-window.onload = pickRandomWordAndSetHint;
+function main() {
+    pickRandomWordAndSetHint();
+}
+
+//declaring a function named 'checkGuess
+function checkGuess(e) {
+    e.preventDefault();
+    const letterInput = document.getElementById("letter-guess");
+    const letter = document.getElementById("letter-guess").value.toLowerCase();
+    notification.textContent = "";
+
+    if (letter.length == 0 || !/^[a-z]$/.test(letter)) {
+        notification.textContent = "You MUST enter a valid letter!";
+        return;
+    }
+
+    if (incorrectGuessesList.includes(letter) || correctGuessesList.includes(letter)) {
+        notification.textContent = "You have already tried this letter.";
+        return;
+    }
+
+    if (randomWordLowerCase.includes(letter)) {
+        successfulGuess(letter);
+    } else {
+        incorrectGuess(letter);
+    }
+
+    letterInput.value = "";
+}
+
+function successfulGuess(letter) {
+    correctGuessesList.push(letter);
+    notification.textContent = "Bingo! Good Guess"
+    textContent = document.getElementById('word-display').textContent;
+    newTextContent = "";
+    for (let i = 0; i < randomWordLowerCase.length; i++) {
+        if (randomWordLowerCase[i] == letter) {
+            newTextContent += randomWordOriginalCase[i];
+        } else {
+            newTextContent += textContent[i];
+        }
+    }
+    document.getElementById('word-display').textContent = newTextContent;
+
+    if (!newTextContent.includes('_')) {
+        endGame(true);
+    }
+}
+
+function incorrectGuess(letter) {
+    incorrectGuessesList.push(letter);
+    notification.textContent = "Incorrect guess : " + letter;
+    let incorrectGuessesSpan = document.querySelector("#incorrect-guesses span");
+
+    // Update its content with the incorrectGuessesList
+    incorrectGuessesSpan.textContent = incorrectGuessesList.join(', '); // This will display the letters separated by commas
+    if (incorrectGuessesList.length >= MAX_INCORRECT_GUESSES) {
+        endGame(false);
+    }
+    currentImageIndex++;
+    if (currentImageIndex >= imageList.length) {
+        return;
+    }
+    document.getElementById("game-image").src = imageList[currentImageIndex];
+}
+
+function endGame(isWinner) {
+    document.getElementById("letter-guess").disabled = true;
+    document.getElementById("enter-button").disabled = true;
+    if (isWinner) {
+        notification.textContent = "Congratulations! You win!";
+    } else {
+        notification.textContent = "Game Over! The word was " + randomWordOriginalCase;
+        document.getElementById("letter-guess").disabled = true;
+        document.getElementById("enter-button").disabled = true;
+    }
+}
